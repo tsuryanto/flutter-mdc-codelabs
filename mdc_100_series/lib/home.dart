ccf305bcbc921e0c425649d0879191a65fd4bdf8
tsuryanto <tsuryanto16@gmail.com>
2022-03-09 21:29:09 +0700

    Membuat GridView Layout

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -18,16 +18,80 @@ class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   // TODO: Make a collection of cards (102)
+
+  List<Card> _buildGridCards(int count) {
+    return List.generate(
+      count,
+      (index) => Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset('assets/diamond.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.00, 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text('Title'),
+                  SizedBox(height: 8.0),
+                  Text('Secondary Text'),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
-      // TODO: Add app bar (102)
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+    return Scaffold(
+      appBar: AppBar(
+        // TODO: Add buttons and title (102)
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu Button');
+          },
+        ),
+        title: const Text('SHRINE'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('Search Button');
+            },
+            icon: const Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              print('Filter Button');
+            },
+            icon: const Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+          ),
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: _buildGridCards(10),
       ),
       resizeToAvoidBottomInset: false,
     );
