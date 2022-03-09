e028810277a90010d27199ee229ddf73a355429b
tsuryanto <tsuryanto16@gmail.com>
2022-03-09 15:29:29 +0700

    My Solution for 101 - Build Login Page

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -22,7 +22,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,8 +44,41 @@ class _LoginPageState extends State<LoginPage> {
             // TODO: Wrap Username with AccentColorOverride (103)
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
-            // TODO: Add TextField widgets (101)
-            // TODO: Add button bar (101)
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+              controller: _passwordController,
+            ),
+            const SizedBox(height: 12.0),
+            ButtonBar(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    _usernameController.clear();
+                  },
+                  child: const Text('CANCEL'),
+                ),
+                // TODO: Add an elevation to NEXT (103)
+                // TODO: Add a beveled rectangular border to NEXT (103)
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('NEXT'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
